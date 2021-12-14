5774baf676864d7fa0d756a93bce12a1187ead4e
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2021-12-14 15:13:48 -0300

    It was added the ability to save data on database

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import {UnauthorizedError} from '../errors/UnauthorizedError'; 
+
+
+@Injectable()
+export class UsersService {
+
+   constructor(private readonly prisma: PrismaService) {}
+
+  create(data: CreateUserDto) {
+    return this.prisma.user.create({
+      data
+    });
+  }
+
+  findAll() {
+    throw new UnauthorizedError('Custom message')
+    return this.prisma.user.findMany();
+  }
+
+
+}
